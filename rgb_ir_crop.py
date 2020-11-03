1c6dd1453fd1f90e18fe81a84d963cbc50aeca35
Siddhant Jagtap <siddhantjagtap@Siddhants-MacBook-Pro.local>
2020-11-02 23:44:25 -0500

    changed rgb2ir

--- rgb_ir_crop.py
+++ rgb_ir_crop.py
@@ -29,3 +29,5 @@ for pair in img_pairs:
         # Read, crop and write the IR disparity image:
         ir_disp = cv.imread('IR_disp/'+str(pair[1])+'.tiff',cv.IMREAD_UNCHANGED)
         cv.imwrite('CROPPED/IR/'+ str(pair[1])+ '.tiff',ir_disp[98:421,84:513])
+
+#
